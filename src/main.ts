8930fd92ff2a9bca1fe5567b509a9f0044ca388a
analuizamelodev <analuizamelodasvirgens@gmail.com>
2026-06-11 17:42:17 -0300

    feat: add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Track System API')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger UI is available on: ${await app.getUrl()}/api`);
 }
 bootstrap();
